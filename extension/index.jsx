80ad71a9b3ba8d5dff703e2dddbb7c601f8c8c77
Aaron Farber <aaronfarber9@gmail.com>
2017-07-24 01:10:18 -0700

    first commit

---
+++ extension/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
+
+document.addEventListener(('DOMContentLoaded'), () => {
+  let store = configureStore();
+  const root = document.getElementsByTagName('html');
+  ReactDOM.render(<Root store={ store }/>, root);
+});
